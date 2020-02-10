015561bbb1ebbfea40c5574f3a2466255a768396
Tomasz <tomek.dziopa@gmail.com>
2020-02-10 21:44:47 +0100

    home snapshot async graphene handlers missing

--- api/run.py
+++ api/run.py
@@ -1,16 +1,32 @@
-import graphene
+import graphene as gp
 from fastapi import FastAPI
 from starlette.graphql import GraphQLApp
 
 from databases import Database
 
-
-class Query(graphene.ObjectType):
-    hello = graphene.String(name=graphene.String(default_value="stranger"))
-
-    def resolve_hello(self, info, name):
-        return "Hello! ch " + name
-
+database = Database('postgresql://db:5432', user='postgres', password='test')
 
 app = FastAPI()
-app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
+
+class Places(gp.ObjectType):
+    async_places = gp.String(description='places', start_date=gp.DateTime(), end_date=gp.DateTime())
+
+    async def resolve_async_places(self, info, start_date, end_date):
+        print(start_date)
+        print(end_date)
+        rows = await database.fetch_all(query='''SELECT * FROM places where last_review >= :start_date AND last_review < :end_date''' )
+        print(rows)
+        return "xd"
+
+
+
+
+app.add_route("/", GraphQLApp(schema=gp.Schema(query=Places)))
